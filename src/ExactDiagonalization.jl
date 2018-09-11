de80136fc071f79e3ebdf84bea5947c83c83336e
JacobCJLin <jacob0425@gmail.com>
2018-09-11 14:41:43 -0700

    Rename the files
    renaming ExactDiag.jl to ED.jl
    modify the header file accordingly

--- src/ExactDiagonalization.jl
+++ src/ExactDiagonalization.jl
@@ -2,7 +2,7 @@ module ExactDiagonalization
 
 using LinearAlgebra
 
-include("ExactDiag.jl")
+include("ED.jl")
 
 
 export EDtable,printstate,num2basis,basis2num,tran,Op_translation, inversion,evolution
