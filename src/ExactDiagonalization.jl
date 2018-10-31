d587c0b4efdccfdd96b6091a05c1f852416ce26c
JacobCJLin <jacob0425@gmail.com>
2018-10-31 10:00:12 -0700

    Add export EDdata function
    Add export EDdata

--- src/ExactDiagonalization.jl
+++ src/ExactDiagonalization.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 include("ED.jl")
 include("Entanglement.jl")
 
-export EDtable,printstate,num2basis,basis2num,tran,Op_translation, inversion,evolution
+export EDtable,printstate,num2basis,basis2num,tran,Op_translation, inversion,evolution,EDdata
 export GenerateED,GenerateEDK,GenerateEDKI,findrep
 export Svon,Sparsity,EntSpectrum
 
