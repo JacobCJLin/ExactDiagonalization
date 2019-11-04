637376725d6825b3bac2762393394497d82ff8dd
JacobCJLin <jacob0425@gmail.com>
2019-11-04 13:18:26 -0500

    Update ED.jl

--- src/ED.jl
+++ src/ED.jl
@@ -125,13 +125,14 @@ end
 #function for calculate ED evolution
 function evolution(E,S,Δt)
     eiEt=exp.(-1.0im*Δt*E)
-    dim1,dim2=size(S)
-    U=zeros(complex(Float64),dim1,dim1)
-    for i=1:dim1, j=1:dim1
-       for k=1:dim2
-        U[i,j]+= S[i,k]*eiEt[k]*conj(S[j,k])
-        end
-    end
+    Ut=S*Diagonal(eiEt)*S'
+    #dim1,dim2=size(S)
+    #U=zeros(complex(Float64),dim1,dim1)
+    #for i=1:dim1, j=1:dim1
+    #   for k=1:dim2
+    #    U[i,j]+= S[i,k]*eiEt[k]*conj(S[j,k])
+    #    end
+    #end
     return U
 end
 
