dc1aec815e1b2d94a9161f46232e5fc3f7ee5b6d
JacobCJLin <jacob0425@gmail.com>
2019-12-10 16:02:33 -0500

    update

--- src/ED.jl
+++ src/ED.jl
@@ -19,6 +19,35 @@ for i=1:dim
 end    
 end
 
+#Loading EDtable data if exist; if not, construct it
+function EDtabledata(L,symf,statecheck,filename)
+    if isfile(filename)
+    EDfile=open(filename,"r")
+    ED=deserialize(EDfile)
+    close(EDfile)  
+    else
+    println("no file: Construct EDtable.")
+    ED=generateEDsym(L,symf,statecheck=statecheck)
+    EDfile=open(filename,"w")
+    serialize(EDfile,ED)
+    close(EDfile)
+    end    
+    return ED
+end
+
+#Loading EDtabledata if exist
+function loadEDtabledata(EDfilename)
+    if isfile(filename)
+    EDfile=open(filename,"r")
+    ED=deserialize(EDfile)
+    close(EDfile)
+    return ED 
+    else
+    println("no file: ",EDfilename)
+    end
+end
+
+
 #Loading EDdata if exist; if not, diagonalize it
 function EDdata(H,EDfilename)
     if isfile(EDfilename)
@@ -59,7 +88,6 @@ function loadEDdata(EDfilename)
 end
 
 #functions for ED
-#functions
 function codetobasis(a::Int64,L::Int64,base=2) #convert a state code a to basis, e.g. 1=0000001
     basis=zeros(Int64,L);
     temp=a;
@@ -186,128 +214,6 @@ function generateED(L::Int64,base=2;statecheck= i -> true)
 end
 
 
-
-##################################################################################
-#### Old
-##################################################################################
-
-
-
-#generate states with momentum resolution
-function GenerateEDK(L::Int64,K::Int64,base=2;tol=1E-14,statecheck= i -> true)
-    fulldim=base^L
-    counter=0;
-    state=Dict()
-    index=Dict()
-    Snorm=Dict()
-    for i=0:fulldim-1
-      if statecheck(i)  
-        leaststate=i;
-        tempstate=i;
-        for r=1:L-1   #loop over the translational operation; if the state is the smallest then it's representative
-            tempstate=translation_code(tempstate,L,base);
-            tempstate<leaststate ? leaststate=tempstate : nothing
-        end
-         if leaststate==i
-            #determine it's statenorm
-            tempnorm=1;
-            tempstate=i;
-            for r=1:L-1
-                tempstate=translation_code(tempstate,L,base);
-                tempstate==i ? tempnorm+=exp(2im*π*K*r/L) : nothing
-            end
-            
-            if abs(tempnorm)>tol  #count the state if statenorm!=0
-            counter+=1
-            state[counter]=i;
-            index[i]=counter;
-            Snorm[counter]=sqrt(tempnorm)
-            end
-          end
-        end  #end statecheck  
-    end
-    dim=counter
-    ED=EDtable(state,index,Snorm,dim,base,L)
-    return ED
-end
-
-#generate states with momentum resolution + inversion symmetry resolution
-function GenerateEDKI(L::Int64,K::Int64,Inv::Bool,base=2;tol=1E-12,statecheck= i -> true)
-    fulldim=base^L
-    Inv ? Isgn=1 : Isgn=-1;  #sign of the inversion symmetry
-    counter=0;
-    state=Dict()
-    index=Dict()
-    Snorm=Dict()
-    for i=0:fulldim-1
-        if statecheck(i)
-        leaststate=i;
-        tempstate=i;
-        Itempstate=inversion_code(tempstate,L,base)
-        Itempstate<leaststate ? leaststate=Itempstate : nothing
-        for r=1:L-1   #loop over the translational operation; if the state is the smallest then it's representative
-            tempstate=translation_code(tempstate,L,base);
-            tempstate<leaststate ? leaststate=tempstate : nothing
-            Itempstate=inversion_code(tempstate,L,base)   #checked the inversion symmetry
-            Itempstate<leaststate ? leaststate=Itempstate : nothing
-        end
-         if leaststate==i
-            #determine it's statenorm
-            tempnorm=1;
-            tempstate=i;
-            Itempstate=inversion_code(tempstate,L,base)
-            Itempstate==i ? tempnorm+=Isgn : nothing
-            for r=1:L-1
-                tempstate=translation_code(tempstate,L,base);
-                tempstate==i ? tempnorm+=exp(2im*π*K*r/L) : nothing
-                Itempstate=inversion_code(tempstate,L,base)
-                Itempstate==i ? tempnorm+=Isgn*exp(2im*π*K*r/L) : nothing
-            end
-            
-            if abs(tempnorm)>tol  #count the state if statenorm!=0
-            counter+=1
-            state[counter]=i;
-            index[i]=counter;
-            Snorm[counter]=sqrt(tempnorm)
-            end
-          end
-        end
-    end
-    dim=counter
-    ED=EDtable(state,index,Snorm,dim,base,L)
-    return ED
-end
-
-function findrep(a::Int64,K::Int64,ED::EDtable,base=2)
-    L=ED.L
-    tempstate=a;
-    for r=0:L-1
-        if haskey(ED.index,tempstate)
-            return tempstate,exp(2im*π*r*K/L) 
-        else
-            tempstate=translation_code(tempstate,L,base)
-        end
-    end
-    return -1,0
-end
-
-function findrep(a::Int64,K::Int64,Inv::Bool,ED::EDtable,base=2)
-    L=ED.L
-    Inv ? Isgn=1 : Isgn=-1;  #sign of the inversion symmetry
-    tempstate=a;
-   
-    for r=0:L-1
-            Itempstate=inversion_code(tempstate,L,base)
-        if haskey(ED.index,tempstate)
-            return tempstate,exp(2im*π*r*K/L) 
-        elseif haskey(ED.index,Itempstate)
-            return Itempstate,Isgn*exp(2im*π*r*K/L)    
-        else
-            tempstate=translation_code(tempstate,L,base)
-        end
-    end
-    return -1,0
-end
 
 
 
--- src/ExactDiagonalization.jl
+++ src/ExactDiagonalization.jl
@@ -5,9 +5,9 @@ using LinearAlgebra, Serialization
 include("ED.jl")
 include("Entanglement.jl")
 
-export EDtable,printstate,codetobasis,basistocode,num2basis,basis2num
+export EDtabledata,loadEDtabledata,EDtable,printstate,codetobasis,basistocode,num2basis,basis2num
 export translation_code,translation,inversion_code,inversion,evolution,loadEDdata,EDdata
-export generateEDsym,findrepsym,generateED,GenerateEDK,GenerateEDKI,findrep
+export generateEDsym,findrepsym,generateED
 export reshapelist,Svon_sym,Svon,Sparsity,EntSpectrum
 
 end
