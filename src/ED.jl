7c6826e5b0129c36afd822ac51aaf7a7cc393dd7
JacobCJLin <jacob0425@gmail.com>
2022-03-15 10:06:58 -0500

    Update ED.jl

--- src/ED.jl
+++ src/ED.jl
@@ -1,39 +1,37 @@
 #type for ED calculation
 struct EDtable
-    state::Dict
-    index::Dict
-    normsq;
-    dim::Int64
+    state::Dict  #given an index, output the state code
+    index::Dict  #given a state code, output its index
+    normsq;      #The norm square of the state
+    dim::Int64   #dimension of the Hilbert space
     base::Int64
     L
 end
+
+
+function genHmat(Hcode,ED::EDtable)
+    #Hcode: takes a computational basis |a>  and generate H|a>
+    dim=ED.dim
+    Hmat=zeros(dim,dim)
+    for i=1:dim
+        outvec=Hcode(ED.state[i],ED)
+        Hmat[:,i]=outvec
+    end
+    return Hmat
+end
+
+
 #some tools
-function printstate(state,ED::EDtable;tol=1E-12)
+function printstate(state,ED::EDtable;tol=1E-12) #printour the state with nonzero amplitudes
 dim=ED.dim
 L=ED.L
 base=ED.base
-for i=1:dim
-    if abs(state[i])>tol
-        println(state[i],"\t",codetobasis(ED.state[i],L,base))
-    end
-end    
+    for i=1:dim
+        if abs(state[i])>tol
+            println(state[i],"\t",codetobasis(ED.state[i],L,base))
+        end
+    end    
 end
-
-#Loading EDtable data if exist; if not, construct it
-#function EDtabledata(L,symf,statecheck,filename)
-#    if isfile(filename)
-#    EDfile=open(filename,"r")
-#    ED=deserialize(EDfile)
-#    close(EDfile)  
-#    else
-#    println("no file: Construct EDtable.")
-#    ED=generateEDsym(L,symf,statecheck=statecheck)
-#    EDfile=open(filename,"w")
-#    serialize(EDfile,ED)
-#    close(EDfile)
-#    end    
-#    return ED
-#end
 
 #Loading EDtable data if exist; if not, construct it
 function EDtabledata(L,symf,statecheck,filename)
@@ -129,83 +127,14 @@ end
 
 basis2num(basis::Array{Int64},base=2)=basistocode(basis,base)
 
-#translation operation
-function translation_code(a::Int64,L::Int64,base=2)
-    basis=codetobasis(a,L,base)
-    Tbasis=zeros(Int64,L);
-    Tbasis[1]=basis[L];
-    Tbasis[2:L]=basis[1:L-1];
-    return basistocode(Tbasis,base)
-end
-
-function translation(vec,ED)
-    outvec=zeros(eltype(vec),ED.dim)
-    for i=1:ED.dim
-       a=ED.state[i]
-       Ta=translation_code(a,ED.L,ED.base)
-       outvec[ED.index[Ta]]+=vec[i]  
-    end
-    return outvec
-end
-
-#space inversion operation
-function inversion_code(a::Int64,L::Int64,base=2)
-   oldarray=codetobasis(a,L,base)
-   newarray=zeros(Int64,L)
-    for i=1:L
-    newarray[i]=oldarray[L-i+1]
-    end
-    return basistocode(newarray,base)
-end
-
-function inversion(vec,ED)
-    outvec=zeros(eltype(vec),ED.dim)
-    for i=1:ED.dim
-       a=ED.state[i]
-       Ia=inversion_code(a,ED.L,ED.base)
-       outvec[ED.index[Ia]]+=vec[i]  
-    end
-    return outvec 
-end
 
 #function for calculate ED evolution
 function evolution(E,S,Δt)
     eiEt=exp.(-1.0im*Δt*E)
     U=S*Diagonal(eiEt)*S'
-    #dim1,dim2=size(S)
-    #U=zeros(complex(Float64),dim1,dim1)
-    #for i=1:dim1, j=1:dim1
-    #   for k=1:dim2
-    #    U[i,j]+= S[i,k]*eiEt[k]*conj(S[j,k])
-    #    end
-    #end
     return U
 end
 
-#function generateEDsym(L::Int64,symf,base=2;tol=1E-14,statecheck= i -> true)
-   #L: total number of sites
-   #symf: function for the symmetry operation, requiring input ("code", L, base), output ("codelist","χlist")  
-#    fulldim=base^L
-#    counter=0;
-#    state=Dict();index=Dict();normsq=Dict(); 
-#    for i=0:fulldim-1
-#        if statecheck(i) 
-#            codelist,χlist=symf(i,L,base)
-#            minimum(codelist)!= i ? continue : nothing
-#            indlist=findall(x -> x==i , codelist)
-#            snormsq=sum(χlist[indlist])
-#            if abs(snormsq) > tol
-#            counter+=1;
-#            state[counter]=i;
-#            index[i]=counter;
-#            normsq[counter]=snormsq    
-#            end
-#        end 
-#    end
-#    dim=counter
-#    ED=EDtable(state,index,normsq,dim,base,L)
-#    return ED
-#end
 
 function generateEDsym(L::Int64,symf,base=2;tol=1E-14,statecheck= i -> true,symfile = nothing)
    #L: total number of sites
@@ -283,7 +212,47 @@ function generateED(L::Int64,base=2;statecheck= i -> true)
     return ED
 end
 
+#commonly used symmtry operations in 1d
+#---------------------
+#translation operation
+function translation_code(a::Int64,L::Int64,base=2)
+    basis=codetobasis(a,L,base)
+    Tbasis=zeros(Int64,L);
+    Tbasis[1]=basis[L];
+    Tbasis[2:L]=basis[1:L-1];
+    return basistocode(Tbasis,base)
+end
 
+function translation(vec,ED)
+    outvec=zeros(eltype(vec),ED.dim)
+    for i=1:ED.dim
+       a=ED.state[i]
+       Ta=translation_code(a,ED.L,ED.base)
+       outvec[ED.index[Ta]]+=vec[i]  
+    end
+    return outvec
+end
+
+#space inversion operation
+function inversion_code(a::Int64,L::Int64,base=2)
+   oldarray=codetobasis(a,L,base)
+   newarray=zeros(Int64,L)
+    for i=1:L
+    newarray[i]=oldarray[L-i+1]
+    end
+    return basistocode(newarray,base)
+end
+
+function inversion(vec,ED)
+    outvec=zeros(eltype(vec),ED.dim)
+    for i=1:ED.dim
+       a=ED.state[i]
+       Ia=inversion_code(a,ED.L,ED.base)
+       outvec[ED.index[Ia]]+=vec[i]  
+    end
+    return outvec 
+end
+#--------------------------------------------
 
 
 
